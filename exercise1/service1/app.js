757c0224b7d1dd87008f376962d1600349fa1a92
Konsta Jokinen <konstajokinen01@gmail.com>
2025-09-28 14:40:59 +0300

    add communication between services and write records to vStorage

--- exercise1/service1/app.js
+++ exercise1/service1/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const checkDiskSpace = require("check-disk-space").default
+const fs = require('fs');
 
 const secondsToHours = (seconds) => {
     const date = new Date(null)
@@ -9,16 +10,36 @@ const secondsToHours = (seconds) => {
     return date.toISOString().substring(11, 19)
 }
 
+const createRecord = async () => {
+    const timeStamp = new Date().toISOString().split(".")[0] + "Z"
+    const upTime = secondsToHours(process.uptime())
+    const diskSpace = await checkDiskSpace("/") 
+    const freeSpaceMB = diskSpace.free / (1024*1024)
+    return `${timeStamp}: uptime ${upTime} hours, free disk in root: ${freeSpaceMB.toFixed(2)} MBytes`
+}
+
+const getRecord = async () => {
+    const response = await fetch("http://service2:5000/status")
+    const data = await response.text()
+    return data
+}
+
+
 app.get("/", function(req, res) {
     return res.send("Hello World!");
 });
 
 app.get("/status", async function(req, res) {
-    const timeStamp = new Date().toISOString().split(".")[0] + "Z"
-    const upTime = secondsToHours(process.uptime())
-    const diskSpace = await checkDiskSpace("/") 
-    const freeSpaceMB = diskSpace.free / (1024*1024)
-    return res.send(`${timeStamp}: uptime ${upTime} hours, free disk in root: ${freeSpaceMB.toFixed(2)} MBytes`);
+    const record1 = await createRecord()
+    // await POSTrecordToStorage()
+    fs.appendFileSync("/data/records.txt", record1 + "\n")
+
+    const record2 = await getRecord()
+
+    console.log("record1", record1)
+    console.log("record2", record2)
+    
+    return res.type("text").send(`${record1}\n${record2}`);
 });
 
 app.get("/log", function(req, res) {
--- exercise1/service2/app.py
+++ exercise1/service2/app.py
@@ -16,16 +16,24 @@ def getFreeDiskSpace():
     total, used, free = shutil.disk_usage("/")
     return free / (1024*1024)
 
+def createRecord():
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+    uptime = time.time() - startTime
+    freeDiskSpace = getFreeDiskSpace()
+    return f"{timestamp}: uptime {secondsToHours(uptime)} hours, free disk in root: {freeDiskSpace:.2f} MBytes"
+
 @app.route("/")
 def home():
     return "Welcome to Flask with Docker!"
 
 @app.route("/status")
 def status():
-    timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
-    uptime = time.time() - startTime
-    freeDiskSpace = getFreeDiskSpace()
-    return f"{timestamp}: uptime {secondsToHours(uptime)} hours, free disk in root: {freeDiskSpace:.2f} MBytes"
+    record = createRecord()
+    
+    with open("/data/records.txt", "a") as f:
+        f.write(record + "\n")
+
+    return record
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
